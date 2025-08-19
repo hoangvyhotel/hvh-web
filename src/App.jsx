436cb4ac7b67d33c0abb3a76fb7732ae05107a9c
DatNT <datnguyen151004@gmail.com>
2025-08-19 22:28:35 +0700

    add tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { RouterProvider } from 'react-router-dom';
 import ThemeCustomization from './themes';
 
 import router from 'routes';
-
+import 'index.css';
 function App() {
   return (
     <ThemeCustomization>
--- vite.config.mjs
+++ vite.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import jsconfigPaths from 'vite-jsconfig-paths';
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
@@ -23,6 +24,6 @@ export default defineConfig(({ mode }) => {
       global: 'window'
     },
     base: APP_BASE_URL,
-    plugins: [react(), jsconfigPaths()]
+    plugins: [react(), jsconfigPaths(), tailwindcss()]
   };
 });

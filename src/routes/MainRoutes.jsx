ec3c2097827545912c375351a19b9bacb2eca34a
DatNT <datnguyen151004@gmail.com>
2025-08-23 18:22:30 +0700

    feat(UpdatePrice_expense) update price for rooms and expense management

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -4,10 +4,11 @@ import { lazy } from 'react';
 import Loadable from 'components/Loadable';
 import MainLayout from 'layouts/MainLayout';
 import RoomManagement from '../views/pages/management/room-management';
+import ExpenseManagement from '../views/pages/management/expense-management/expense-page';
 import HotelManagementLayout from '../layouts/HotelManagementLayout';
-import CostManagement from '../views/pages/management/cost-management';
 import RetalManagement from '../views/pages/management/rental-management';
 import ChangePassword from '../views/pages/management/change-password';
+import RoomPriceUpdate from 'views/pages/management/price-management/update-price-page';
 
 // pages
 const DashboardDefault = Loadable(lazy(() => import('views/dashboard/default')));
@@ -48,16 +49,18 @@ const MainRoutes = {
           element: <RoomManagement />
         },
         {
-          path: 'cost-management',
-          element: <CostManagement />
-        },
-        {
           path: 'rental-management',
           element: <RetalManagement />
         },
         {
           path: 'change-password',
-          element: <ChangePassword />
+          element: <ChangePassword />,
+          path: 'expense-management',
+          element: <ExpenseManagement />
+        },
+        {
+          path: 'price-management',
+          element: <RoomPriceUpdate />
         }
       ]
     },

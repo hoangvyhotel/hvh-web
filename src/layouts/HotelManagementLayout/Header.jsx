e5e0a026d5f0fbc5158b9feb14a0f8cf5fbc098c
devzeus <hieu01bdvn@gmail.com>
2025-08-23 21:21:28 +0700

    update ui utitity

--- src/layouts/HotelManagementLayout/Header.jsx
+++ src/layouts/HotelManagementLayout/Header.jsx
@@ -3,7 +3,8 @@ import { NavLink } from 'react-router-dom';
 
 const Header = ({ title, icon }) => {
   return (
-    <header className="lg:pb-6 pb-4 flex">
+    <header className="flex items-center flex-1">
+      {/* Nút quay lại */}
       <NavLink
         to={'/pages/management'}
         className="flex items-center px-4 py-2 bg-gray-200 border rounded-lg space-x-2 hover:bg-gray-300 transition-colors cursor-pointer"
@@ -11,14 +12,17 @@ const Header = ({ title, icon }) => {
         <ArrowLeftToLine className="w-4 h-4 text-gray-600" />
         <span className="font-semibold text-gray-700 hover:text-gray-900">Quay lại</span>
       </NavLink>
+
+      {/* Tiêu đề căn giữa */}
       <div className="flex-1 flex items-center justify-center">
         {icon}
-        <h1 className="font-bold text-2xl" style={{ color: '#348667' }}>
+        <h1 className="font-bold text-2xl text-[#348667] ml-2">
           {title}
         </h1>
       </div>
     </header>
   );
 };
+
 
 export default Header;

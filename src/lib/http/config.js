e9a46e2fc4f4c393a8489d80746e0b424b0ff6dc
DatNT <datnguyen151004@gmail.com>
2025-09-07 09:42:13 +0700

    deploy

--- src/lib/http/config.js
+++ src/lib/http/config.js
@@ -1,5 +1,5 @@
 export const apiConfig = {
-  baseURL: import.meta.env.BASE_URL_API  || "http://localhost:3001/api/v1",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "https://hvh-api-production.up.railway.app/api/v1",
   get now() {
     return Date.now();
   }
